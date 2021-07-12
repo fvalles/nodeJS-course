a485b561ce91d1cd3b002e3c3a3ecd7790ac64da
Federico Valles <fedeva_37@hotmail.com>
2021-07-11 22:12:57 -0300

    dynamic pages with hbs templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -5,6 +5,28 @@ const app = express();
 const publicDirPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirPath));
+app.set("view engine", "hbs");
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Fede Valles",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Fede Valles",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help Page",
+    subtitle: "Do you have any questions? Ask them here! :)",
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({
