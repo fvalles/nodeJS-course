ca0ded82c17fdf6e9df08fbcb652c493ef87e4fa
Federico Valles <fedeva_37@hotmail.com>
2021-07-26 22:22:14 -0300

    The query string

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -42,9 +42,25 @@ app.get("/help", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({ error: "You must provide an address term" });
+  }
+
   res.send({
     forecast: "It's cloudy and it's 10 degrees",
     location: "Buenos Aires",
+    address: req.query.address,
+  });
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({ error: "You must provide a search term" });
+  }
+
+  console.log(req.query);
+  res.send({
+    products: [],
   });
 });
 
