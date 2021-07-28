ba7777429205991e8123a40f0ca88ad3ce968e72
Federico Valles <fedeva_37@hotmail.com>
2021-07-27 23:59:40 -0300

    search form created

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,12 +1,22 @@
 console.log("Client side JavaScript file is loaded!");
 
-fetch("http://localhost:3000/weather?address=dusseldorf")
-  .then((response) => response.json())
-  .then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
-    }
-  });
+const weatherForm = document.querySelector("form");
+
+const search = document.querySelector("input");
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const address = search.value;
+
+  fetch(`http://localhost:3000/weather?address=${address}`)
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.error) {
+        console.log(data.error);
+      } else {
+        console.log(data.location);
+        console.log(data.forecast);
+      }
+    });
+});
